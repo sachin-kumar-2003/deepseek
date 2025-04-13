12e7aab06645545c57afd9494d7492ffdbc377a6
sachin kumar <goputen215@gmail.com>
2025-04-13 11:27:46 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
-
-const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const clerkFrontendApi = 'pk_test_Zmlyc3Qtd2hhbGUtMy5jbGVyay5hY2NvdW50cy5kZXYk'
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={clerkFrontendApi}>
