c21e710539b891dccb1807b54a78a33535fcd029
sachin kumar <goputen215@gmail.com>
2025-04-15 19:48:30 +0530

    Update PromptBox.jsx

--- src/components/PromptBox.jsx
+++ src/components/PromptBox.jsx
@@ -8,7 +8,8 @@ import { useAppContext } from '../context/AppContext';
 import { GoogleGenAI } from "@google/genai";
 import ChatMessages from './ChatMessages';
 import { useUser } from '@clerk/clerk-react';
-const googleApiKey ='AIzaSyDdT1ICJDFN_tFd2NLDkAyxi8ivr-pN90E'; // Ensure you have the API key set in your environment variables
+// const googleApiKey ='AIzaSyDdT1ICJDFN_tFd2NLDkAyxi8ivr-pN90E'; // Ensure you have the API key set in your environment variables
+const googleApiKey=import.meta.env.VITE_GOOGLE_API_KEY; // Ensure you have the API key set in your environment variables
 console.log(googleApiKey); // Log the API key for debugging purposes
 const PromptBox = ({ setIsLoading, isLoading }) => {
   const [prompt, setprompt] = useState('');
